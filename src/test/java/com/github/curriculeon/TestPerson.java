36c2b497d552333f6718af0607cb5a7def666621
JakeLawhorne <jake.lawhorne@gmail.com>
2024-03-07 15:11:00 -0500

    finished Person class, and testPerson

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,37 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+    @Test
+    public void testConstructor(){
+        //given
+        long expectedId = 101;
+        String expectedName = "Jake";
+
+        //when
+        Person person = new Person(expectedId, expectedName);
+        long actualId = person.getId();
+        String actualName = person.getName();
+
+        //then
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+    @Test
+    public void testSetName(){
+        //given
+        String expectedName = "Jake";
+        Person person = new Person(null, null);
+        Assert.assertNotEquals(expectedName, person.getName());
+
+        //when
+        person.setName(expectedName);
+        String actualName = person.getName();
+
+        //then
+        Assert.assertEquals(expectedName, actualName);
+    }
 
 }
